5128415930905176b0b866472800a7617808757d
sudeepsnairails <sudeepsnair.career@gmail.com>
2023-08-30 16:08:56 -0400

    layout changes

--- config/initializers/mapkick.rb
+++ config/initializers/mapkick.rb
@@ -1,1 +1,3 @@
 ENV["MAPBOX_ACCESS_TOKEN"] = "pk.eyJ1Ijoic3VkaXBzbiIsImEiOiJjbGxyMmtmMnQwajYwM29ucnZzbXBmNzB4In0.8ArxUMTmif-CV4hOxKaY7w"
+Mapkick.options[:height] = "500px"
+Mapkick.options[:width] = "500px"
